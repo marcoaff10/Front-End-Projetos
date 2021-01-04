13278fd6adc4d3093d12163e05f7e8d41c543721
marcoaff10 <marcoafilho36@gmail.com>
2021-01-04 15:36:45 -0300

    Projetos novos

---
+++ Projeto_11/index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+    <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.no-icons.min.css" rel="stylesheet">
+    <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Projeto_11</title>
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div class="logo">
+                <a href=""><img src="images/logo.png" alt=""></a>
+                </div><!--logo-->
+
+            <div class="menu">
+                <a href="">Sobre</a>
+                <a href="">Depoimentos</a>
+                <a class="btn-cta" href="">Baixar Agora!</a>
+            </div><!--menu-->
+
+            <div class="clear"></div>
+        </div><!--container-->
+    </header>
+
+    <section class="topo">
+        <div class="container">
+            <div class="texto-topo">
+                <h2>Desafie seus amigos nesse multiplayer repleto de aventura</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Nulla molestie urna sed felis tempor, varius malesuada dui malesuada. 
+                Cras vehicula mattis nibh. Maecenas tincidunt tempus elit, 
+                et vestibulum felis feugiat ut. Curabitur consectetur justo eu nibh sodales, 
+                at eleifend neque viverra.</p>
+                <br>
+                <a href=""><img src="images/appstore.png" alt=""></a>
+                <a href=""><img src="images/googleplay.png" alt=""></a>
+            </div><!--texto-topo-->
+            <div class="img-topo">
+                <img src="images/hero.jpg" alt="">
+            </div><!--img-topo-->
+        </div><!--container-->
+    </section><!--topo-->
+
+    <section class="depoimentos">
+        <div class="container">
+            <div class="depoimento-single">
+                <p>"O melhor jogo do ano! Incrível como consigo aprender e me divertir ao mesmo tempo!"</p>
+                <br>
+                <span>Marco Antonio - Maf Code</span>
+            </div><!--depoimento-single-->
+
+            <div class="depoimento-single">
+                <p>"O melhor jogo do ano! Incrível como consigo aprender e me divertir ao mesmo tempo!"</p>
+                <br>
+                <span>Marco Antonio - Maf Code</span>
+            </div><!--depoimento-single-->
+        </div><!--container-->
+    </section><!--depoimentos-->
+
+    <section class="sobre">
+        <div class="container">
+            <div class="titulo-sobre">
+                <h2>Sobre</h2>
+            </div><!--titulo-sobre-->
+            <div class="flex">
+            <div class="sobre-single">
+                <div class="titulo-single">
+                    <i class="icon-user"></i>
+                    <h3>MultiPlayer</h3>
+                </div><!--titulo-single-->
+
+                <div class="sobre-conteudo">
+                    Divirta-se com seus amigos e veja quem é o melhor da rodada!
+                    <p></p>
+                </div><!--sobre-conteudo-->
+            </div><!--sobre-single-->
+
+            <div class="sobre-single">
+                <div class="titulo-single">
+                    <i class="icon-user"></i>
+                    <h3>MultiPlayer</h3>
+                </div><!--titulo-single-->
+
+                <div class="sobre-conteudo">
+                    Divirta-se com seus amigos e veja quem é o melhor da rodada!
+                    <p></p>
+                </div><!--sobre-conteudo-->
+            </div><!--sobre-single-->
+
+            <div class="sobre-single">
+                <div class="titulo-single">
+                    <i class="icon-user"></i>
+                    <h3>MultiPlayer</h3>
+                </div><!--titulo-single-->
+
+                <div class="sobre-conteudo">
+                    Divirta-se com seus amigos e veja quem é o melhor da rodada!
+                    <p></p>
+                </div><!--sobre-conteudo-->
+            </div><!--sobre-single-->
+            </div><!--flex-->
+        </div><!--container-->
+    </section><!--sobre-->
+
+    <footer>
+        <div class="container">
+
+        </div><!--container-->
+    </footer>
+</body>
+</html>
